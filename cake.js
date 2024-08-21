701f4a84f16b66602c896f7a9dcfecad6f8825e1
otavioing <162048765+otavioing@users.noreply.github.com>
2024-08-21 15:20:54 -0300

    Add files via upload

---
+++ cake.js
@@ -0,0 +1,23 @@
+// este arquivo javascript serve para abrir e fechar o menu lateral do site
+
+window.addEventListener('load', paginacarregada); 
+
+function paginacarregada() {
+
+
+
+    let botaomenu = document.getElementById("botaoMenu");
+    botaomenu.addEventListener("click", abrirMenu);
+
+    function abrirMenu() {
+        if(menu.style.right == "-100%" || menu.style.right == "") {
+            menu.style.right = "0";
+        } else {
+           if(menu.style.right = "0"){
+            menu.style.right = "-100%";
+           }
+        }
+        
+    }
+
+}
